ba5cd3cb69b26177868b2ccff22108961a5cef55
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-08-13 14:23:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-//your JS code here. If required.
-document.getElementById('enterBtn').addEventListener('click', ()=>{
-	// let h1 = document.createElement('h1');
-	// h1.innerText = "Entered Metaverse"
-	// document.getelementById("head").append("h1");
-	let pTag = document.getElementById("status")
-	pTag.style.fontWeight = "15px"
-	
-	pTag.innerText = "Entered Metaverse";
-	
-})
+function enter(){
+	document.getElementById('status').innerHTML = '<h1>Entered Metaverse</h1>';
+}
