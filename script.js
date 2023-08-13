e3d91ec807c5903ae39e127cbcf0bf9246327bc5
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-08-13 14:01:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //your JS code here. If required.
 document.getElementById('enterbtn').addEventListener('click', ()=>{
 	let h1 = document.creatElement('h1');
-	
+	h1.innerText = "Entered Metaverse"
+	document.getelementById("head").append(h1);
+	document.getElementById("status").innerText = "";
 	
 })
